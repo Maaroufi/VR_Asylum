8d2554af914a364ff01d3236ec1a5649fb254af5
Maaroufi <34818183+Maaroufi@users.noreply.github.com>
2020-04-03 12:00:47 +0100

    Updated 4th

---
+++ Assets/Scripts/BezierActivation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BezierActivation : MonoBehaviour {
+
+    public GameObject obj;   //here the object you want to disable
+    public GameObject objUI;
+    public OVRInput.Button menu = OVRInput.Button.PrimaryIndexTrigger;
+    private bool activity;
+
+    void Start()
+    {
+        activity = false;
+    }
+
+    void Update()
+    {
+        obj.SetActive(activity);   //here disables the obj
+
+        if (OVRInput.GetDown(menu))
+        {
+            if (activity == true)
+            {
+                activity = false;
+            }
+            else
+            {
+                activity = true;
+            }
+        }  
+    }
+}
