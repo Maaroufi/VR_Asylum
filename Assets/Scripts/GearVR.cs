8f6022c534672078f07d77cbc1cdf6adba1237eb
Maaroufi <34818183+Maaroufi@users.noreply.github.com>
2020-10-01 02:07:40 +0100

    Updated 4th

---
+++ Assets/Scripts/GearVR.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearVR : MonoBehaviour {
+
+    public Renderer rend;
+
+    void Start () {
+        rend = GetComponent<Renderer>();
+        rend.enabled = false;
+    }
+	
+	void Update () {
+		
+	}
+}
